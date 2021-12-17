8386561a791b32d3ce118f5361f801d82f796d6d
cesarvargasjr <cesar_vargas_jr@hotmail.com>
2021-12-17 20:53:35 -0300

    Módulo 6

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,17 +1,23 @@
 import Customer from "../core/Customer";
+import { IconEdition, IconTrash } from "./Icons";
 
 interface TableProps {
     clientes: Customer[]
+    customerSelect?: (cliente: Customer) => void
+    customerExclude?: (cliente: Customer) => void
 }
 
 export default function Table(props: TableProps) {
 
+    const exibirAcoes = props.customerExclude || props.customerSelect
+
     function renderizarCabecalho() {
         return (
             <tr>
-                <th>Id</th>
-                <th>Nome</th>
-                <th>Idade</th>
+                <th className="text-left p-3">Id</th>
+                <th className="text-left p-3">Nome</th>
+                <th className="text-left p-3">Idade</th>
+                {exibirAcoes ? <th className="p-3">Action</th> : false}
             </tr>
         )
     }
@@ -19,18 +25,48 @@ export default function Table(props: TableProps) {
     function renderizarDados() {
         return props.clientes?.map((cliente, i) => {
             return (
-                <tr key={cliente.id}>
-                    <td>{cliente.id}</td>
-                    <td>{cliente.nome}</td>
-                    <td>{cliente.idade}</td>
+                <tr key={cliente.id}
+                    className={`${i % 2 === 0 ? 'bg-blue-200' : 'bg-blue-100'}`}>
+                    <td className="text-left p-2">{cliente.id}</td>
+                    <td className="text-left p-2">{cliente.nome}</td>
+                    <td className="text-left p-2">{cliente.idade}</td>
+                    {exibirAcoes ? renderizarAcoes(cliente) : false}
                 </tr>
             )
         })
     }
 
+    function renderizarAcoes(cliente: Customer) {
+        return (
+            <td className="flex justify-center">
+                {props.customerSelect ? (
+                    <button onClick={() => props.customerSelect?.(cliente)} className={`
+                        flex justify-center items-center
+                        text-green-600 rounded-full p-2 m-1
+                        hover:bg-blue-50
+                    `}>
+                        {IconEdition}
+                    </button>
+                ) : false}
+                {props.customerExclude ? (
+                    <button onClick={() => props.customerExclude?.(cliente)} className={`
+                        flex justify-center items-center
+                        text-red-600 rounded-full p-2 m-1
+                        hover:bg-blue-50
+                        `}>
+                        {IconTrash}
+                    </button>
+                ) : false}
+            </td>
+        )
+    }
+
     return (
-        <table>
-            <thead>
+        <table className="w-full rounded-xl overflow-hidden">
+            <thead className={`
+                text-gray-50
+                bg-gradient-to-r from-blue-400 to-blue-600
+            `}>
                 {renderizarCabecalho()}
             </thead>
             <tbody>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,19 @@ export default function Home() {
     new Cliente('1', 'Cesar', 26),
     new Cliente('2', 'Gabriel', 25),
     new Cliente('3', 'Ingrid', 27),
+    new Cliente('4', 'Camila', 24),
+    new Cliente('5', 'Teresa', 60),
   ]
+
+  /* FUNÇÕES PARA EXIBIR OS BOTÕES/AÇÕES, CRIADA COM MESMO NOME */
+  function customerSelect(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function customerExclude(cliente: Cliente) {
+    console.log(`Excluindo... ${cliente.nome}`)
+  }
+
 
   return (
     <div className={`
@@ -16,8 +28,11 @@ export default function Home() {
       bg-gradient-to-r from-blue-400 to-blue-600
       text-white
     `}>
-      <Layout title="Cadastro">
-        <Table clientes={clientes}></Table>
+      <Layout title="Cadastro de Clientes">
+        <Table clientes={clientes}
+          customerSelect={customerSelect}
+          customerExclude={customerExclude}
+        />
       </Layout>
     </div>
   )
